523b2d4d0728614099dab3d6292c83c7020c898a
Elena <myone72@yahoo.com>
2021-12-05 21:13:12 -0500

    create Historical games repository with ready index page

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$(function() {
+	$(window).on("scroll", function() {
+		 if($(window).scrollTop() > 50) {
+			  $(".header").addClass("_changeBgc");
+			  $(".header__link_active").addClass("_changeColor");
+			  $(".header__link").addClass("_changeHover");
+		 } else {
+			 $(".header").removeClass("_changeBgc");
+			 $(".header__link_active").removeClass("_changeColor");
+			 $(".header__link").removeClass("_changeHover");
+		 }
+	});
+});
+
+
+$(document).ready(function(){
+	$('.item-events__item-one').click(function(event){
+		$('.item-events__title-one').toggleClass('open-block');
+		$('.item-events__text-one').toggleClass('display-block');
+	});
+});
+$(document).ready(function(){
+	$('.item-events__item-two').click(function(event){
+		$('.item-events__title-two').toggleClass('open-block');
+		$('.item-events__text-two').toggleClass('display-block');
+	});
+});
+$(document).ready(function(){
+	$('.item-events__item-three').click(function(event){
+		$('.item-events__title-three').toggleClass('open-block');
+		$('.item-events__text-three').toggleClass('display-block');
+	});
+});
+$(document).ready(function(){
+	$('.item-events__item-four').click(function(event){
+		$('.item-events__title-four').toggleClass('open-block');
+		$('.item-events__text-four').toggleClass('display-block');
+	});
+});
