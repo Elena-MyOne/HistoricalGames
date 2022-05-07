7e63dc770e3bffef81bac431ebe95d81ec2b29bb
Elena <myone72@yahoo.com>
2022-05-07 18:02:08 -0400

    update header navigation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(document).ready(function(){
-	$('.header__burger').click(function(event){
-		$('.header__burger, .header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
+//burger menu==========================================================
+const headerBurger = document.querySelector(".header__burger");
+const headerMenu = document.querySelector(".header__menu");
+const headerList = document.querySelector(".header__list");
+
+headerBurger.onclick = function () {
+	headerBurger.classList.toggle('active');
+	headerMenu.classList.toggle('active');
+	document.body.classList.toggle('lock')
+}
+
+headerList.addEventListener("click", function(event) {
+   if( event.target.closest(".header__link")) {
+      headerBurger.classList.remove('active');
+      headerMenu.classList.remove('active');
+      document.body.classList.remove('lock');
+   }
 });
+
 
 $(function() {
 	$(window).on("scroll", function() {
